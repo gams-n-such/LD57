097dce281b88acce0504188eb6fca9ae6d4858d4
Denis Maximenko <manlok71@gmail.com>
2025-04-07 23:59:59 +0400

    Implement credits screen

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -1,7 +1,9 @@
 class_name MainMenu
 extends Control
 
+@export var tutorial_screen_scene : PackedScene
 @export var config_screen_scene : PackedScene
+@export var credits_screen_scene : PackedScene
 var config_screen : Control = null
 
 func _on_play_button_pressed() -> void:
@@ -10,3 +12,6 @@ func _on_play_button_pressed() -> void:
 func _on_config_button_pressed() -> void:
 	config_screen = config_screen_scene.instantiate() as Control
 	add_child(config_screen)
+
+func _on_credits_button_pressed() -> void:
+	add_child(credits_screen_scene.instantiate())
